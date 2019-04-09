f8e4e5afaa588a6fb249685df50f9d3c7dcd11b5
Ryan P. Brewster <RyanPBrewster@gmail.com>
2019-04-08 23:18:07 -0700

    Initial commit, HTTP server with client-configurable delayed responses

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+# Use the offical Golang image to create a build artifact.
+# This is based on Debian and sets the GOPATH to /go.
+# https://hub.docker.com/_/golang
+FROM golang:1.12 as builder
+
+# Copy local code to the container image.
+WORKDIR /go
+COPY src .
+
+RUN CGO_ENABLED=0 GOOS=linux go build -v -o foolproof.io/helloworld
+
+# Use a Docker multi-stage build to create a lean production image.
+# https://docs.docker.com/develop/develop-images/multistage-build/#use-multi-stage-builds
+FROM alpine
+
+# Copy the binary to the production image from the builder stage.
+COPY --from=builder /go/helloworld /foolproof/bin/helloworld
+
+# Run the web service on container startup.
+CMD ["/foolproof/bin/helloworld"]
---
+++ src/foolproof.io/helloworld/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"sync/atomic"
+	"time"
+)
+
+const ADDRESS = "localhost:8080"
+
+var OPEN_CONNECTIONS int32 = 0
+
+func delayHandler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("%d connections", atomic.AddInt32(&OPEN_CONNECTIONS, 1))
+	defer func() {
+		log.Printf("%d connections", atomic.AddInt32(&OPEN_CONNECTIONS, -1))
+	}()
+
+	values, ok := r.URL.Query()["delay"]
+	if ok {
+		if dur, err := time.ParseDuration(values[0]); err == nil {
+			time.Sleep(dur)
+		}
+	}
+	fmt.Fprintf(w, "Hello, World!\n")
+}
+
+func main() {
+	log.Printf("listening on %s...", ADDRESS)
+	http.HandleFunc("/hello", delayHandler)
+	log.Fatal(http.ListenAndServe(ADDRESS, nil))
+}
