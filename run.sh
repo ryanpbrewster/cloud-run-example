302c7e4f2374744cb6ef5769892fc866130823b1
Ryan P. Brewster <RyanPBrewster@gmail.com>
2019-04-09 00:24:14 -0700

    Accept a port from an environment variable

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-sudo docker run -p 8080:80 gcr.io/foolproof-io/helloworld
+sudo docker run --env PORT=80 -p 8080:80 gcr.io/foolproof-io/helloworld
--- src/foolproof.io/helloworld/main.go
+++ src/foolproof.io/helloworld/main.go
@@ -9,14 +9,12 @@ import (
 	"time"
 )
 
-const ADDRESS = ":80"
-
-var OPEN_CONNECTIONS int32 = 0
+var connections int32 = 0
 
 func delayHandler(w http.ResponseWriter, r *http.Request) {
-	log.Printf("%d connections", atomic.AddInt32(&OPEN_CONNECTIONS, 1))
+	log.Printf("%d connections", atomic.AddInt32(&connections, 1))
 	defer func() {
-		log.Printf("%d connections", atomic.AddInt32(&OPEN_CONNECTIONS, -1))
+		log.Printf("%d connections", atomic.AddInt32(&connections, -1))
 	}()
 
 	values, ok := r.URL.Query()["delay"]
@@ -33,12 +31,13 @@ func inspectHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		hostname = "UNKNOWN"
 	}
-	fmt.Fprintf(w, "%d open connections on %s!\n", OPEN_CONNECTIONS, hostname)
+	fmt.Fprintf(w, "%d open connections on %s!\n", connections, hostname)
 }
 
 func main() {
-	log.Printf("listening on %s...", ADDRESS)
+	port := os.Getenv("PORT")
+	log.Printf("listening on localhost:%s...", port)
 	http.HandleFunc("/hello", delayHandler)
 	http.HandleFunc("/inspect", inspectHandler)
-	log.Fatal(http.ListenAndServe(ADDRESS, nil))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 }
